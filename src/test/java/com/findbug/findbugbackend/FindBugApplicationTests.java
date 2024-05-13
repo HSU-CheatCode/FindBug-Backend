c9b5afb835aaa9af5e0367d5d6f043cd3b05fe37
D15M4S <122260287+D15M4S@users.noreply.github.com>
2024-05-13 22:38:48 +0900

    🎉 초기 설정 세팅 및 프로젝트 생성

---
+++ src/test/java/com/findbug/findbugbackend/FindBugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findbug.findbugbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindBugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
